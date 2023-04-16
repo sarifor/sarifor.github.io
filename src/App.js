5544b44f9eaa34ddf3909c304c93259a5454e3ec
sarifor <iread1q84@gmail.com>
2023-04-16 17:45:38 +0900

    Scroll to top: App/ScrollBox components are combined

--- src/App.js
+++ src/App.js
@@ -112,13 +112,39 @@ class App extends Component {
     )
   };
   
+  scrollToTop = () => {
+    // const { scrollHeight, clientHeight } = this.box;
+    this.box.scrollTop = 0;
+  }
+
   render() {
+    const style = {
+      border: '1px solid black',
+      height: '300px',
+      width: '300px',
+      overflow: 'auto',
+      position: 'relative'
+    }
+
+    const innerStyle = {
+      width: '100%',
+      height: '650px',
+      background: 'linear-gradient(white, black)'
+    }
+
     const { videos } = this.state;
 
     return (
       <div>
         <p>Video Info Collector</p>
-        {videos ? this._renderVideos() : "Loading..."}
+        <div style={style} ref={(ref) => {this.box=ref}}> {/* ScrollBox Component */}
+          <div style={innerStyle}>
+           {videos ? this._renderVideos() : "Loading..."}
+          </div>
+        </div>
+        <button onClick={() => this.scrollToTop()}>
+          Go to Top
+        </button>
       </div>
     )
   }
